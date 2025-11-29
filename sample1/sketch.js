1ea4c3cd52f664dfe16237d4822f93766b22330e
rangtee <jonglee2003@gmail.com>
2025-11-29 13:53:43 +0900

    Add files via upload

---
+++ sample1/sketch.js
@@ -0,0 +1,74 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(20); 
+  
+  fill('#DB3827')
+  arc(0, 0, 350, 350, radians(0), radians(90))
+  
+   fill('#DB7727')
+  noStroke()
+  arc(0, 0, 250, 250, radians(0), radians(90))
+  
+  fill('#DB5928')
+  noStroke()
+  arc(0, 0, 150, 150, radians(0), radians(90))
+  
+  fill('#DB2C5E')
+  noStroke()
+  arc(0, 0, 50, 50, radians(0), radians(90))
+  
+  
+  fill('#20DB26')
+  noStroke()
+  arc(600, 0, 340, 340, radians(90), radians(180))
+  
+  fill('#21DB66')
+  noStroke()
+  arc(600, 0, 240, 240, radians(90), radians(180))
+  
+  fill('#67DB21')
+  noStroke()
+  arc(600, 0, 140, 140, radians(90), radians(180))
+  
+  fill('#21DBA6')
+  noStroke()
+  arc(600, 0, 40, 40, radians(90), radians(180))
+  
+  
+  fill('#2D1DDB')
+  noStroke()
+  arc(0, 400, 350, 350, radians(270), radians(360))
+  
+  fill('#1D49DB')
+  noStroke()
+  arc(0, 400, 250, 250, radians(270), radians(360))
+  
+  fill('#1D85DB')
+  noStroke()
+  arc(0, 400, 150, 150, radians(270), radians(360))
+  
+  fill('#6B1DDB')
+  noStroke()
+  arc(0, 400, 50, 50, radians(270), radians(360))
+  
+  
+  fill(250)
+  noStroke()
+  arc(600, 400, 340, 340, radians(180), radians(270))
+  
+  fill(200)
+  noStroke()
+  arc(600, 400, 240, 240, radians(180), radians(270))
+  
+  fill(150)
+  noStroke()
+  arc(600, 400, 140, 140, radians(180), radians(270))
+  
+  fill(100)
+  noStroke()
+  arc(600, 400, 40, 40, radians(180), radians(270))
+  
+}
